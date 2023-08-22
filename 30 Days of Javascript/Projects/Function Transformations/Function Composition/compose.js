b63508a56abb1cfb8d6a53f925f2a77792e9a09a
cl9nt <muthunguclintn@gmail.com>
2023-08-22 06:50:49 +0300

    updated on 21/08/23

--- 30 Days of Javascript/Projects/Function Transformations/Function Composition/compose.js
+++ 30 Days of Javascript/Projects/Function Transformations/Function Composition/compose.js
@@ -1,8 +1,7 @@
 var compose = function(functions) {
-    return functions(x) {
+    return functions(x);
         for (let i = functions.length -1; i >= 0; i--) {
             x = functions[i](x);
         }
         return x;
-    };
 };
